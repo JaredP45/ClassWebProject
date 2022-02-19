70e36dc361b6b75591dec898b993725e832657ae
JaredP45 <52184578+JaredP45@users.noreply.github.com>
2022-02-18 21:48:58 -0600

    Added HTML for templates.

--- base/LandingPage/views.py
+++ base/LandingPage/views.py
@@ -4,9 +4,13 @@ from .models import UpperSection, MidSection, LowerSection
 
 def base(request):
     context = {
-        'greeting': UpperSection.objects.all(),
         'about': MidSection.objects.all(),
         'team': LowerSection.objects.all(),
     }
 
     return render(request, 'base.html', context)
+
+
+def upper(request):
+    context = {'greeting': UpperSection.objects.all()}
+    return render(request, 'UpperSection.html', context)
