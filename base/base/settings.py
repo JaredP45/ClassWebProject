0daa151a0ffe65c2d6cbb264ce55212858f937e5
JaredP45 <synetos@protonmail.com>
2022-04-01 19:27:12 -0500

    Add crispy forms to installed apps and add bootstrap template pack.

--- base/base/settings.py
+++ base/base/settings.py
@@ -40,8 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'LandingPage',
 ]
+
+# Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
